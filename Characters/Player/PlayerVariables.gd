8f08fb8b10eea3a751a50306c54593fab402f13e
Ethan Major <Ethan Major>
2023-10-25 16:31:20 -0400

    Implemented moving platform

--- Characters/Player/PlayerVariables.gd
+++ Characters/Player/PlayerVariables.gd
@@ -4,4 +4,3 @@ var health : float = 100
 var jump_velocity : float = -200
 var double_jump_velocity : float = -150
 var key_count : float = 0
-const default_pos : Vector2 = Vector2(-21, 33)
--- Managers/PositionManager.gd
+++ Managers/PositionManager.gd
@@ -2,11 +2,5 @@ extends Area2D
 
 @export var pos_to_move_to : Vector2
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_body_entered(body):
+	body.position = pos_to_move_to
